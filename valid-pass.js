8c5bc943c1a77d948be59e5ee7679fdf26b679c4
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-06-10 13:17:35 +0530

    Create valid-pass.js

---
+++ valid-pass.js
@@ -0,0 +1,14 @@
+function CheckPassword(inputtxt) 
+{ 
+var passw=  /^[A-Za-z]\w{7,14}$/;
+if(inputtxt.value.match(passw)) 
+{ 
+alert('Correct, try another...')
+return true;
+}
+else
+{ 
+alert('Wrong...!')
+return false;
+}
+}
