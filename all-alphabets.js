7606483480cefc6544a878fa20ddb3914f22007d
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-06-10 13:12:00 +0530

    Create all-alphabets.js

---
+++ all-alphabets.js
@@ -0,0 +1,14 @@
+function allLetter(inputtxt)
+  {
+   var letters = /^[A-Za-z]+$/;
+   if(inputtxt.value.match(letters))
+     {
+      return true;
+     }
+   else
+     {
+     alert("message");
+     return false;
+     }
+  }
+  
