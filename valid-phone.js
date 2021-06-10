034e99ae4da7554ab07dd65de087e5f904ac58ee
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-06-10 13:17:10 +0530

    Create valid-phone.js

---
+++ valid-phone.js
@@ -0,0 +1,13 @@
+function phonenumber(inputtxt)
+{
+  var phoneno = /^\d{10}$/;
+  if((inputtxt.value.match(phoneno))
+        {
+      return true;
+        }
+      else
+        {
+        alert("message");
+        return false;
+        }
+}
