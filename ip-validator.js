fab906a00aa384c2135844ad02cf6c7e070b285e
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-06-10 13:18:01 +0530

    Create ip-validator.js

---
+++ ip-validator.js
@@ -0,0 +1,9 @@
+function ValidateIPaddress(ipaddress) 
+{
+ if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+alert("You have entered an invalid IP address!")
+return (false)
+}
