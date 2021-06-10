45c2de3f0d6abb8006166639fa514c5e09155a5d
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-06-10 13:16:39 +0530

    Create valid-uid.js

---
+++ valid-uid.js
@@ -0,0 +1,11 @@
+function userid_validation(uid,mx,my)
+{
+var uid_len = uid.value.length;
+if (uid_len == 0 || uid_len >= my || uid_len < mx)
+{
+alert("User Id should not be empty / length be between "+mx+" to "+my);
+uid.focus();
+return false;
+}
+return true;
+}
